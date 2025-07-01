4503a055980144931c3936960b704d52e2ab234a
Jos3PM <just76111@hotmail.com>
2025-07-01 10:42:08 -0500

    Delete RegHost directory

--- RegHost/src/main/java/upeu/edu/pe/reghost/repositorio/PacientesRepositorio.java
+++
@@ -1,8 +0,0 @@
-package upeu.edu.pe.reghost.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upeu.edu.pe.reghost.entidades.Pacientes;
-
-public interface PacientesRepositorio extends JpaRepository <Pacientes, Long> {
-
-}
